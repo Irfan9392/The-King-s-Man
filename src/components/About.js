9cee274878ad01ecb2b087d2d359b983b0c40128
Irfan <mohammed134@gannon.edu>
2024-06-07 11:41:20 -0400

    Update gh-pages with the latest code from main

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h2>About Us</h2>
+      {/* Add content for about us here */}
+    </div>
+  );
+};
+
+export default About;
